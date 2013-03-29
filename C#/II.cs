fa09e7f1a84fe917f455f78e1e3acad30f6c14c6
Kasia <hasiek01@gmail.com>
2013-03-29 21:47:39 +0100

    Aplikacja do obsługi faz snu - wersja konsolowa

---
+++ C#/II.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime now = DateTime.Now;
+            DateTime firstOption = now.AddHours(6.5);
+            DateTime secondOption = now.AddHours(8);
+            DateTime thirdOption = now.AddHours(9.5);
+
+            Console.WriteLine("Oh, so you want to go to bed? Great idea!");
+            Console.Write("You should wake up at " + firstOption.ToShortTimeString());
+            Console.Write(" or " + secondOption.ToShortTimeString());
+            Console.WriteLine(" or " + thirdOption.ToShortTimeString() + '.');
+            Console.Read();
+        }
+    }
+}
