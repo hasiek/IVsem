b688f14fcad3b84e28b29710ecd031b44d452250
Kasia <hasiek01@gmail.com>
2013-04-07 21:21:06 +0200

    Okienkowe fazy snu - wszystkie funkcjonalności

--- C#/III.cs
+++ C#/III.cs
@@ -26,14 +26,60 @@ namespace SleepPhase1
         {
             string input;
             string[] inputs;
+            string output = "You can go to bed at:\n";
+            int i = 0;
             int numberOfHours, numberOfMinutes;
             input = textBox.Text;
             inputs = (input.Split(':'));
             numberOfHours = Convert.ToInt32(inputs[0]);
             numberOfMinutes = Convert.ToInt32(inputs[1]);
-            DateTime hour = new DateTime(2013, 12, 12, numberOfHours, numberOfMinutes, 0);
-            MessageBox.Show(hour.Hour.ToString() + ':' + hour.Minute.ToString());
+            for (i = 0; i < 6; i++)
+            {
+                numberOfHours -= 1;
+                numberOfMinutes -= 30;
+                if (numberOfMinutes < 0)
+                {
+                    numberOfHours--;
+                    numberOfMinutes = 60 + numberOfMinutes;
+
+                }
+                if (numberOfHours > 24) numberOfHours %= 24;
+                if (numberOfHours < 0) numberOfHours = 24 + numberOfHours;
+
+                if (numberOfMinutes >= 10) output += Convert.ToString(numberOfHours) + ':' + Convert.ToString(numberOfMinutes) + '\n';
+                else output += Convert.ToString(numberOfHours) + ':' + Convert.ToString(numberOfMinutes) + "0\n";
+            }
+            MessageBox.Show(output, "SleepyTeller Answer", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            int i = 1;
+            int hour, minute;
+            hour = now.Hour;
+            minute = now.Minute;
+            int[,] hours = new int[7,2];
+            string output = "You can wake up at:\n";
+
+            for (i = 0; i < 6; i++)
+            {
+                hour += 1;
+                minute += 30;
+                if (minute >= 60)
+                {
+                    hour++;
+                    minute %= 60;
+
+                }
+                if (hour > 24) hour %= 24;
+
+                if (minute >= 10) output += Convert.ToString(hour) + ':' + Convert.ToString(minute) + '\n';
+                else output += Convert.ToString(hour) + ':' + Convert.ToString(minute) + "0\n";
+            }
+
+            MessageBox.Show(output, "SleepyTeller Answer", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
         }
     }
 }
